76d1e02d72bea73056e4366d73ae524b1d0914d7
Bill Nguyen <william.nguyen@gm.com>
2022-08-09 13:39:20 -0700

    #1000 add prod env

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket         = "billhome-tf-08012022"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "billhome-tf"
   }
 }
